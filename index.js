e5cf6349c607038756e3369308733f3399792b1c
Ankur Chakravarty <ankurchaks@gmail.com>
2020-06-24 12:00:00 +0530

    FirstUpload

---
+++ index.js
@@ -0,0 +1,74 @@
+
+       
+      document.getElementById("Selectstory").onchange=function(){
+         
+             
+         //Get the value of the selection   
+        x=document.getElementById("combobox1").value;
+      
+
+          //alert(x)
+          if (x=='1'){
+              document.getElementById('storyTitle').textContent='The Fox And The Crow';
+              document.getElementById('storyImage').src="images/image1.jpg";
+              document.getElementById('content').innerHTML="Long ago, in a jungle there was a cunning fox. Once While the fox was passing under a tree he saw a crow, who had a very tempting piece of cheese in his beak.<br><br>The fox got an idea. He stopped and said to the crow, \"Dear crow, your feathers are so beautiful, and you look so pretty. I have heard that your voice is very sweet and that you sing really well. Will you please do the favour of singing a song for me?\" <br><br>The crow was so happy to hear his praise that he immediately opened his mouth to sing and, in the process, dropped the piece of cheese. The fox caught the cheese and ate it. When the crow asked back for his piece of cheese, the fox said, \"You\'ve eaten my praise for lunch. You don\'t need it anymore.\" The stupid crow realized his mistake.<br><br>He knew he was not so good at singing and yet, his foolish ego fell for the praise made by the fox."
+          }
+          else if (x=='2') {
+            document.getElementById('storyTitle').textContent='The Lion And The Hare';
+            document.getElementById('storyImage').src="images/image2.1.jpg";
+            document.getElementById('content').innerHTML="Once upon a time there lived a cruel lion in a forest. He used to kill animals without any reason. All the animals told the lion to stop killing them, as they shall send an animal to hm everyday as his meal. The lion agreed.<br><br>Then on, everyday one animal was sent as lion\'s food. One day, came the turn of a clever hare.<br><br>The hare, however did not reach on time. When the lion inquired, he replied. \"There is another lion in the jungle who wanted to kill me.\" The lion got furious and asked the hare to take him to the other lion. The hare took him to a well. There the lion saw his own reflection in the water and mistook it as the oher lion. The lion wanted to be the only king, so he jumped in the well to kill the other lion, but drowned and died.<br><br>Thus, the clever rabbit got rid of the lion, who was ten times mighter than him, by his intelligence and confidence."
+          } 
+          else if (x=='3') {
+            document.getElementById('storyTitle').textContent='The Monkey And The Bell';
+            document.getElementById('storyImage').src="images/image3.jpg";
+            document.getElementById('content').innerHTML="Once upon a time a thief stole a bell from the temple. That thief was hubted down by a tiger. In the process the bell fell on the ground. The monkeys saw the bell and carried it home. From that day onwards the monkeys used to ring the bell every night. The noise scared the villagers. One day they discovered the body of the thief and believed that an evil spirit is haunting the village.<br><br>Only one womenhad the guts to find out what was making the noise, and eentured inside the forest. She saw the monkeys were rringing the bell. The women was an opportunist. She went upto the king and told him thatshe could get rid of the spirit by having a prayer. For this purpose she took huge amount pf money from the king. Son she got the bell from the monkeys and kept the money to herself!<br><br>It was cleaverness and fearlessness that not only helped her tackle the problem but also make profit out of it."
+          } 
+          else if (x=='4') {
+            document.getElementById('storyTitle').textContent='The Rabbit And The Totoise';
+            document.getElementById('storyImage').src="images/image4.jpg";
+            document.getElementById('content').innerHTML="Once upon a time there lived a rabbit, who was very vain. He was very proud about the fact that he could run very fast. One day a tortoise challenged him to race. The rabbit wondered how such a slow animal could beat him, and accepted the challenge.<br><br>Next day, the race began and the rabbit covered a great distance in no time. He saw that the tortoise lagged way behind. Knowing that he had enough time he stopped to rest under a shaddy tree. He didn\'t realise when he fell asleep. At the same time tortoise continues his journey slowly and steadily. When the rabbit woke up, he ran fast towards the finisging point. But, it was too late. He saw the tortoise ahead of him, crossing the finishing line.<br><br>It was then that the rabbit realised that speed is not the only important criteria for winning a race. One should possess the right attitude and should be determined and hardworking as well."
+          } 
+          else if (x=='5') {
+            document.getElementById('storyTitle').textContent='The Wise Elephant';
+            document.getElementById('storyImage').src="images/image5.jpg";
+            document.getElementById('content').innerHTML="Once a group of eleplants, due to scarcity of water, were forced to leave their territory and migrate to a nearby place, where water was available in abundance. They decided to stay in that area.<br><br>The rabbits who inhabited that area got highly worried as their water was now being consumed by the elephants. Moreever, a lot of tem were being squashed under the elephants\' feet.<br><br>One day. the leader of the rabbits went to the elephants\'leader. The elephants\' leader, on hearing this, felt very guilty and sorry. He decided to leave immediately with this group. He could have overlooked the problem faced by the rabbits due to their migrating, and chosen to remain in he the same territory, but he decided not to do so. He respected the fact that the area first belonged to the rabbits. Thus, the wise elephant took the right decision and saved the rabbits from their worries."
+          }
+          else if (x=='6') {
+            document.getElementById('storyTitle').textContent='The Monkey\'s Heart';
+            document.getElementById('storyImage').src="images/image6.jpg";
+            document.getElementById('content').innerHTML="There lived a monkey who was friends with a crocodile. He used to give him mangoes from the tree. The crocodile found them very delicious. Once he took the mangoes from his wife. When she ate the mangoes she thought, \"The monkeys who feeds on these mangoes must be much more delicious.\" She decided to eat the monkey and percuaded her husband to invite him for dinner to their house so that she can eat him.<br><br>Next day the crocodile invited the monkey. The monket who did not know how to swim. sat on his back. On the way, the crocodile revealed to the monkey about his wife's plan. The clever monkey said,\"Dear friend, you should have told this to me before as I don\'t carry my heart with me. It is in my house. If you want I can get it for your wife to eat.\" The foolish crocodile believed him. When he came near the monkey'\s tree, the monkey jumped onto the tree and saved his life."
+          }
+          else if (x=='7') {
+            document.getElementById('storyTitle').textContent='The Talkative Turtle';
+            document.getElementById('storyImage').src="images/image7.jpg";
+            document.getElementById('content').innerHTML="Once upon a time there lived a turtle in a pond. He was friends with two swans who used to come to the same pond to have water. they spent good time together, talking to each other. The swans were very wise, unlike the turtle who wasvery talkative. He was very impatient and used to cut short their conversation.<br><br>Once a drout hit that area and the pond began drying up. The turtle got very worried, and so did the two swans. The swans came up with an idea. They said to the turtle, \"We can fly you to a new area with plenty of water. For this we will hld the two ends of a stick in our beak and you have to grab the stick with your mount.\" The turtle agreed.<br><br>When they started flying in teh sky, the turtle was surprised to see the scenic view. As soon as he opened his mouth to talk, he fell down and died.<br><br>Thus, it is advised not to talk unecessarity."
+          } 
+          else if (x=='8') {
+            document.getElementById('storyTitle').textContent='The Naughty Monkey';
+            document.getElementById('storyImage').src="images/image8.jpg";
+            document.getElementById('content').innerHTML="There once lived a naughty monkey who never listened to anyone. All day long he kept on hopping from one tree to another. One day he saw a log of wood that was slightly cut in the middle. The small gap emerging on the log was separated by a nail. The monkey, on seeing it, started playing with the log.<br><br>He tried hard to remove the nail from the log. After struggling with it for some time he fianlly removed it. But the moment he did that, the gap in the log closed and his tail got stuck in it. Though he tried hard to pull out his tail from the log, he failed. He started screaming loudly. On hearing his creams all the villagers started rushing towards him. On seeing the villagers coming towards him, the poor monkey was left with no choice, he ran for life with his half cut tail.<br><br>Thus, one should not touch anything belonging to others."
+          } 
+          else if (x=='9') {
+            document.getElementById('storyTitle').textContent='The Cat\'s Justice';
+            document.getElementById('storyImage').src="images/image9.jpg";
+            document.getElementById('content').innerHTML="There lived a sparrow on a tree for nearly seven years. One day she decided to go to her friend\'place for some time.<br><br> After few days when she returned, she was surprixed to see a ribbit living on her tree. On inquiring, the rabbit said that he is new to this place and has been living on this tree for the past seven days, and it\'s his home now. On hearing this the sparow got angry and a fight brike out between the two.<br><br>They saw a cat dressed like a saint passing by and decided to take her help to solve the fight. They told her the entire story. The cat said that she is very old and cannot hear them properly and that they have to come close to her. As soon as he sparrow and the rabbit came near her, she killed them with a stroke and ate them.<br><br>Thus, the two fool\'s fight resulted in a great feast for the cat."
+          } 
+          else if (x=='10') {
+            document.getElementById('storyTitle').textContent='The Stupid Donkey';
+            document.getElementById('storyImage').src="images/image10.gif";
+            document.getElementById('content').innerHTML="In a forest there lived a lion and a fox who were very friendly with each other. Teh lion used to kill the animals and eat them. When he was full, he used to give the remaining of the dead animal to the fox.<br><br>Once the lion fell severely ill. He could not move for many days. The fox top got worried as they were falling short of food. One day, on a river bank, the two friends saw a donkey. They made a plan. The fox went to the donkey and said that he knew a place where there was plenty of green grass. The stupid donkey agreed to accompany the fox.<br><br>The fox took the donkey to the bush where the lion was waiting for him. As soon as the donkey went inside the bush, the lion killed him. Both, the fox and the lion, had a feast.<br><br>The poor donkey had to pay a heavy price for trusting a stranger."
+          } else {
+            alert('Invalid Selection');
+          }
+
+
+          
+        
+    
+        
+      
+    }
+
+
+
+    
